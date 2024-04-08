ff0d8bdaf143490c73c8586c751219296ed4a031
diogtsix <diogtsix@gmail.com>
2024-04-08 22:32:48 +0300

    Created 2 different models NN and SVM for ML model. Added also dataFrame generation

--- solvers/optimization/weight_optimization.py
+++ solvers/optimization/weight_optimization.py
@@ -3,7 +3,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 import numpy as np
+
 from scipy.optimize import minimize
+from scipy.optimize import differential_evolution
+from scipy.optimize import NonlinearConstraint
 
 from solvers.structural_dynamics.preprocessor import Preprocessor
 from solvers.structural_dynamics.solver import Solver
@@ -12,6 +15,19 @@ from solvers.structural_dynamics.postprocess import Postprocess
 from solvers.optimization.material_database import generate_material_np_matrix
 
 from gekko import GEKKO
+from gekko import ML
+from gekko.ML import Gekko_NN_TF,Gekko_LinearRegression
+from gekko.ML import Bootstrap,Conformist,CustomMinMaxGekkoScaler
+
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.neural_network import MLPRegressor
+from tensorflow import keras
+import tensorflow as tf
+
+import pandas as pd 
+
+import matplotlib.pyplot as plt 
  
 class Weight_Optimization():
     
@@ -48,6 +64,8 @@ class Weight_Optimization():
                                                                        'sx')
         
         self.allowableStress = SS*(3/4)
+        self.stress_factor = max(np.abs(self.allowableStress))
+        self.allowableStress = self.allowableStress/self.stress_factor
 
 
     # Objective Function
@@ -63,7 +81,7 @@ class Weight_Optimization():
         #Calc total_weight which should be the output of the objective function
         total_weight = self.calculate_total_weight(updated_elementMatrix)
         
-        print(total_weight)
+        # print(total_weight)
         
         return total_weight
 
@@ -89,8 +107,8 @@ class Weight_Optimization():
         stress = self.extract_structural_attribute_array(structuralProperties, 
                                                                        'sx')
       
-        print(np.min((np.abs(self.allowableStress) - np.abs(stress))  ) )                                                         
-        return np.abs(self.allowableStress) - np.abs(stress)
+        print(np.min((np.abs(self.allowableStress) - np.abs(stress)/self.stress_factor)  ) )                                                         
+        return np.abs(self.allowableStress) - np.abs(stress)/self.stress_factor
 
     # Main Optimization Function
     def run_optimization(self):
@@ -98,13 +116,17 @@ class Weight_Optimization():
         #Solve win for initial point (static solution)
         element_matrix_initial = self.initial_solver.preprocessor.elementMatrix
         num_elements = len(element_matrix_initial) 
+        
         initial_opt_vars, initi_surfaces, init_ids = self.extract_optimization_vars_from_elementMatrix(element_matrix_initial)
         
         initial_point = initial_opt_vars.flatten()  # Initial the starting point for iterations
         
-        bounds_first_half = [(0.5, 4) for _ in range(num_elements)] 
-        bounds_second_half = [(0, 10) for _ in range(num_elements)] 
+        # Create the bounds for opt_vars
+        bounds_first_half = [(0.1, 10) for _ in range(num_elements)] 
+        bounds_second_half = [(0, 2) for _ in range(num_elements)] 
         bounds = bounds_first_half + bounds_second_half 
+        
+
         
         constraints = [{'type': 'ineq', 'fun': self.stress_constraint}
                        ] # The opt algorithm will ensure that ineq will stay >= 0
@@ -118,7 +140,7 @@ class Weight_Optimization():
                             method = 'trust-constr', 
                             bounds = bounds, 
                             constraints = constraints, 
-                            options={'verbose': 3, 'maxiter': 100, 
+                            options={'verbose': 3, 'maxiter': 10, 
                             'initial_tr_radius': 5})
             
         elif self.solverType == 'SLSQP': 
@@ -130,8 +152,16 @@ class Weight_Optimization():
                     bounds=bounds, 
                     constraints=constraints, 
                     options={'ftol': 1e-6, 'eps': 1.5e-4, 'maxiter': 10})
+            
+        # lb =  [(-1) for _ in range(num_elements)] 
+        # ub = [(np.inf) for _ in range(num_elements)] 
+        # nlc = NonlinearConstraint(self.stress_constraint, lb, ub)
         
-        
+        # result = differential_evolution(self.objective_function, bounds, constraints=nlc, 
+        #                                 x0 = initial_point, maxiter = 10, disp = True, 
+        #                                 workers = 1, updating = 'immediate', 
+        #                                 mutation = 0.001)
+ 
         
         # Remove normalization from the surfaces 
         num_of_element = len(element_matrix_initial)
@@ -144,8 +174,106 @@ class Weight_Optimization():
          
         self.results = result
  
-       
+    def run_optimization_ML(self):
         
+        
+        gekkoObj = GEKKO()
+        
+        y_measured, xl, features = self.generate_data(numberOfSamples = 10000)
+
+        data_array = np.concatenate([xl.T, y_measured], axis=1)
+        data = pd.DataFrame(data_array, columns=features + ['y'])
+        label = ['y']
+        
+        ML_model = 'Neural_Network'
+        
+        
+        
+        if ML_model == 'SVR' :
+        
+            #SVR
+            train , test = train_test_split(data,test_size=0.2,shuffle=True)
+            svr = svm.SVR()
+            svr.fit(train[features],np.ravel(train[label]))
+            r2 = svr.score(test[features],np.ravel(test[label]))
+            print('svr r2:',r2)
+        
+        elif ML_model ==  'Neural_Network':
+            # Neural Network
+            X = data.drop('y', axis=1)
+            y = data['y']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            n_features = X_train.shape[1]
+           
+            model = keras.Sequential([
+                keras.Input(shape = (n_features,)),
+                keras.layers.Dense(25, activation='relu'),  # First hidden layer with 128 neurons
+                keras.layers.Dense(25, activation='relu'),  # Second hidden layer with 64 neurons
+                keras.layers.Dense(1)  # Output layer for regression
+            ])
+            
+            model.compile(optimizer = 'adam', 
+                        loss = 'mean_squared_error',
+                        metrics = ['mean_absolute_error']
+                        )
+
+            model.fit(X_train, y_train, epochs=150, batch_size=32, validation_split=0.1)
+
+            test_loss, test_mae = model.evaluate(X_test, y_test)
+            print(f"Test Loss (MSE): {test_loss}, Test MAE: {test_mae}")
+
+            y_pred = model.predict(X_test)
+            
+            
+            # Scatter plot of actual vs. predicted values
+            plt.scatter(y_test, y_pred, alpha=0.5)
+
+            # Line for perfect predictions
+            plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=2)
+
+            plt.xlabel('Actual')
+            plt.ylabel('Predicted')
+            plt.title('Actual vs. Predicted Values')
+            plt.show()
+
+        
+        
+        
+    
+    
+    def generate_data(self, numberOfSamples):
+        
+        elementMatrix = self.initial_solver.preprocessor.elementMatrix
+        
+        mat_size = len(elementMatrix)
+        
+        first_half = np.random.uniform(low=0.001, high=0.01, size=(mat_size, numberOfSamples))
+        self.surface_factor = np.max(first_half)
+        first_half = first_half/self.surface_factor 
+        
+        second_half = np.random.choice([0, 1, 2], size=(mat_size, numberOfSamples))
+
+        samples = np.vstack((first_half, second_half))
+        
+        # Generate the "features" array vector
+        features_first_half = [f"A{i+1}" for i in range(mat_size)]
+        features_second_half = [f"material{i+1}" for i in range(mat_size)]
+        features = features_first_half + features_second_half
+
+        #replace Samples into the elementMatrix
+        y_measured = np.zeros(numberOfSamples)
+        
+        for i in range(numberOfSamples):
+            
+            sample = samples[:,i]
+            
+            updated_elementMatrix = self.replace_optimized_vars_to_elementMatrix(sample, elementMatrix)
+            y_measured[i] = self.calculate_total_weight(updated_elementMatrix)
+        
+        y_measured_reshaped = y_measured.reshape(-1, 1)  # Makes y_measured a 2D array with a single colum
+        
+        return y_measured_reshaped, samples, features 
+    
     def material_constraint(x):
         # Assuming x is an array where the first half represents surfaces
         # and the second half represents material IDs
@@ -261,8 +389,15 @@ class Weight_Optimization():
         FUnction to extract the materil object from my database
         """
         
-        material_id = round(material_id, 4)
+        # material_id = round(material_id, 4)
         
+        if material_id <= 0 :
+            material_id = 0 
+        elif material_id >=2 : 
+            material_id = 2
+        else:
+            material_id = round(material_id)
+            
         for row in self.material_data_base:
             if row[1] == material_id:
                 return row[2]  # Return the Material object
@@ -351,12 +486,17 @@ class Weight_Optimization():
         Z_surfaces = np.zeros([num_vars])    
         
         combined_vector = np.concatenate((Z_surfaces, Z_element.flatten()))
-        print(Z_element)
-        return combined_vector
         
+        return Z_element.flatten()
+    
+  
+        print(f"Current best solution: {intermediate_result.x}")
+        print(f"Objective function value: {intermediate_result.fun}")
+    
 def main():   
     a = Weight_Optimization()
-    a.run_optimization()
-
+    # a.run_optimization()
+    a.run_optimization_ML()
+    
 if __name__ == "__main__":
     main()
